61d3adcbb340c0159243a3390b74d6dfd3e2a6ca
christinazavou <czavou01@gmail.com>
2016-09-27 14:02:26 +0200

    test

--- src/excecute_all.py
+++ src/excecute_all.py
@@ -1,8 +1,12 @@
 
 import os, time
 
+"""
 for c in range(2):
     start_time = time.time()
     runargs = "python main.py ..\\aux_config\\conf" + str(c) + ".yml"
     os.system(runargs)
     print "Finished conf{} after {} minutes.".format(c, (time.time() - start_time) / 60.0)
+"""
+
+print "executing !!"
