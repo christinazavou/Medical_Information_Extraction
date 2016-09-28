407c11afb57883a85377195b8f8250779ceb1af5
christinazavou <czavou01@gmail.com>
2016-09-28 04:17:22 +0200

    test

--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,7 @@ import os
 
 
 def init(configFile, fieldsconfigFile=None, idsconfigFile=None):
+    configFile = os.path.realpath(__file__).replace("\\src\\settings.py", configFile).replace("..","")
     global labels_possible_values
     global ids
     global chosen_labels_possible_values
@@ -26,7 +27,6 @@ def init(configFile, fieldsconfigFile=None, idsconfigFile=None):
     global_settings['path_root_indossiers'] = global_settings['data_path'] + tmp_path_in
     global_settings['path_root_outdossiers'] = global_settings['data_path'] + tmp_path_out
     global_settings['results_path_root'] = doc['results_path_root']
-    global_settings['final_zip_root'] = doc['final_zip_root']
     # ----------------------------------------------excecution config--------------------------------------------------#
     global_settings['read_dossiers'] = doc['read_dossiers']
     global_settings['algo'] = doc['algo']
