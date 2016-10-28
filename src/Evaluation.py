db562cc21d3525047b9398d5812689ad097f8576
christinazavou <czavou01@gmail.com>
2016-10-28 16:36:34 +0200

    evaluation with condition sat

--- src/Evaluation.py
+++ src/Evaluation.py
@@ -4,17 +4,35 @@ Gives Output: Evaluation measure
 i.e. compares outputs with what is on forms
 """
 
+import re
 import string
 import json
 
-import Algorithm
-from ESutils import start_ES, ES_connection
+import algorithms
+import final_baseline
+from ESutils import start_es, EsConnection
 import settings
-import pre_process
-from pre_process import MyPreprocessor
 import time, random
 
 labels_correct_values = {}  # for one patient!!
+
+
+def condition_satisfied(golden_truth, labels_possible_values, current_form, field_to_be_filled):
+    # copied from algorithms
+    # note that values are not pre processed anywhere... i just check them as is
+    # for a given patient(the golden truth) check whether the field to be field satisfies its condition(if exist) or not
+    condition = labels_possible_values[current_form][field_to_be_filled]['condition']
+    if condition == "":
+        return True
+    conditioned_field, condition_expression = re.split(' !?= ', condition)
+    if "!=" in condition:
+        if golden_truth[conditioned_field] != condition_expression:
+            return True
+    elif "==" in condition:
+        if golden_truth[conditioned_field] == condition_expression:
+            return True
+    else:
+        return False
 
 
 class Evaluation:
@@ -27,35 +45,42 @@ class Evaluation:
         self.file = file
         self.chosen_labels_possible_values = chosen_labels_possible_values
         self.chosen_labels_accuracy = {}
+        self.chosen_labels_num = {}  # here i save how many patients were assign a value for that label
+        # since many patients are not assign some label that it's condition is not sat.
         for form in self.chosen_labels_possible_values:
             self.chosen_labels_accuracy[form] = {}
+            self.chosen_labels_num[form] = {}
             for label in self.chosen_labels_possible_values[form]:
                 self.chosen_labels_accuracy[form][label] = 0.0
-
-    """
-    both predictions and targets are of the form: {"label1":"value1","label2":"value2"}
-    """
+                self.chosen_labels_num[form][label] = 0
 
     def eval(self, patients_ids, eval_forms):
+        """
+        both predictions and targets are of the form: {"label1":"value1","label2":"value2"}
+        """
         start_time = time.time()
-        patient_form_pairs = 0
         with open(self.file) as jfile:
             predictions = json.load(jfile, encoding='utf-8')
+
         for patient_id in patients_ids:
             if not (patient_id in predictions.keys()):
                 continue
+
             doc = self.con.get_doc_source(self.index_name, self.type_name_p, patient_id)
             for form_id in eval_forms:
                 if form_id in doc.keys() and form_id in predictions[patient_id].keys():  # double check
-                    patient_form_pairs += 1
-                    patient_form_predictions = predictions[str(patient_id)][form_id]
+                    patient_form_predictions = predictions[patient_id][form_id]
                     patient_form_targets = doc[form_id]
-                    self.accuracy += self.get_score(patient_form_predictions, patient_form_targets, form_id)
-        if patient_form_pairs > 0:
-            self.accuracy = self.accuracy / patient_form_pairs
+                    self.get_score(patient_form_predictions, patient_form_targets, form_id)
+
+            num = 0
             for form_id in eval_forms:
                 for field in self.chosen_labels_accuracy[form_id]:
-                    self.chosen_labels_accuracy[form_id][field] /= patient_form_pairs
+                    self.chosen_labels_accuracy[form_id][field] /= self.chosen_labels_num[form_id][field]
+                    num += 1
+                    self.accuracy += self.chosen_labels_accuracy[form_id][field]
+            self.accuracy /= num
+
         print("score %f" % self.accuracy)
         print("--- %s seconds for eval method---" % (time.time() - start_time))
         return self.accuracy, self.chosen_labels_accuracy
@@ -67,46 +92,44 @@ class Evaluation:
                 print "no predictions"
                 return
             score = 0.0
-            empties = 0
             for field in predictions:
                 if field in chosen_labels:
-                    if targets[field] == "":
-                        empties += 1
-                    if type(predictions[field]) == dict:
-                        if 'value' not in predictions[field].keys():
-                            print "predictions[field] with {} is {}".format(field, predictions[field])
-                        res = predictions[field]['value']
-                    else:
-                        res = predictions[field]
-                    if self.chosen_labels_possible_values[form_id][field]['values'] != "unknown":
-                        # score for : one out of k
-                        if res == targets[field]:
-                            score += 1.0
-                            self.chosen_labels_accuracy[form_id][field] += 1.0
-                    else:
-                        # score for : open-question (BLEU)
-                        res_tokens = res.split(" ")
-                        trgt_tokens = targets[field]
-                        tmp_score = 0
-                        for token in res_tokens:
-                            tmp_score += 1 if (token in trgt_tokens) and not(token in string.punctuation) else 0
-                        tmp_score /= len(res_tokens)
-                        score += tmp_score
-                        self.chosen_labels_accuracy[form_id][field] += tmp_score
-            score /= len(chosen_labels)
-            if random.random() < 0.01:
-                print "a pair score is {} and has {} empty targets out of {} not {}".format(score, empties,
-                                                                                            len(chosen_labels),
-                                                                                            len(predictions))
-            return score
+                    if condition_satisfied(targets, self.chosen_labels_possible_values, form_id, field):
+
+                        if type(predictions[field]) == dict:
+                            if 'value' not in predictions[field].keys():
+                                print "predictions[field] with {} is {}".format(field, predictions[field])
+                            res = predictions[field]['value']
+                        else:
+                            res = predictions[field]
+
+                        self.chosen_labels_num[form_id][field] += 1
+
+                        if self.chosen_labels_possible_values[form_id][field]['values'] != "unknown":
+                            # score for : one out of k
+                            if res == targets[field]:
+                                score += 1.0
+                                self.chosen_labels_accuracy[form_id][field] += 1.0
+                        else:
+                            pass
+                            # # score for : open-question (BLEU)
+                            # res_tokens = res.split(" ")
+                            # trgt_tokens = targets[field]
+                            # tmp_score = 0
+                            # for token in res_tokens:
+                            #     tmp_score += 1 if (token in trgt_tokens) and not(token in string.punctuation) else 0
+                            # tmp_score /= len(res_tokens)
+                            # score += tmp_score
+                            # self.chosen_labels_accuracy[form_id][field] += tmp_score
+                            # for the moment focus on 1-of-k
         except:
             print "some exception in eval score"
-            return 0.0
+
 
 if __name__ == '__main__':
     # start_ES()
-    settings.init("aux_config\\conf14.yml",
-                  "C:\\Users\\Christina Zavou\\Desktop\\results\\")
+    settings.init("aux_config\\conf16.yml",
+                  "C:\\Users\\Christina Zavou\\PycharmProjects\\Medical_Information_Extraction\\results\\")
 
     host = settings.global_settings['host']
     index_name = settings.global_settings['index_name']
@@ -114,8 +137,22 @@ if __name__ == '__main__':
     type_name_f = settings.global_settings['type_name_f']
     type_name_s = settings.global_settings['type_name_s']
     type_name_pp = settings.global_settings['type_name_pp']
-    con = ES_connection(host)
+    con = EsConnection(host)
 
+    eval_file = "C:\\Users\\Christina Zavou\\PycharmProjects\\Medical_Information_Extraction\\results\\" \
+                "conf16_results.json"
+    evaluationsFilePath = "C:\\Users\\Christina Zavou\\PycharmProjects\\Medical_Information_Extraction\\results\\" \
+                          "evaluations.json"
+
+    # note: on type_name_p now
     ev = Evaluation(con, index_name, type_name_p, type_name_f, settings.get_results_filename(),
                     settings.find_chosen_labels_possible_values())
-    a1, a2 = ev.eval(settings.ids['medical_info_extraction patient ids'], settings.global_settings['forms'])
+    score, fields_score = ev.eval(settings.find_used_ids(), settings.global_settings['forms'])
+    evaluations_dict = {}
+    evaluations_dict['evaluation'] += [{'description': settings.get_run_description(), 'file': eval_file,
+                                        'score': score, 'fields_score': fields_score,
+                                        'dte-time': time.strftime("%c")}]
+
+    with open(evaluationsFilePath, 'w') as jfile:
+        json.dump(evaluations_dict, jfile, indent=4)
+    print "Finish evaluating."
