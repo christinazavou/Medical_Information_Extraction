70bf216c529f93ca78a1eb7a41c3af11fbbd1887
christinazavou <czavou01@gmail.com>
2016-10-30 17:54:02 +0100

    if we need more scripts for ES

--- src/make_script.py
+++ src/make_script.py
@@ -1,4 +1,7 @@
 
-class Script():
-    def __init__(self,name):
-        self.name=name
+
+# if we'll need to have a script to query ES make them and save them to scripts folder under ES config
+
+class Script:
+    def __init__(self, name):
+        self.name = name
