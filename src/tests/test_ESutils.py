5a1fd6fad26c0da2ea5e151a1243a2b0a01a6837
christinazavou <czavou01@gmail.com>
2016-11-16 20:31:46 +0100

    nothing

--- src/tests/test_ESutils.py
+++ src/tests/test_ESutils.py
@@ -1,18 +1,18 @@
 import unittest
+from src.ESutils import EsConnection, start_es
 
-from src.ESutils import ES_connection,start_ES
 
 class TestESconnection(unittest.TestCase):
 
     def test_improper_connection(self):
         print "Checks whether exception is raised when connection to ES is not properly established !"
-        self.assertRaises(Exception,s=ES_connection({"host": "localhost", "port": 00}))
+        self.assertRaises(Exception, s=EsConnection({"host": "localhost", "port": 00}))
         print "BAD ONE."
 
     def test_index_creation(self):
-        con = ES_connection({"host": "localhost", "port": 9200})
-        self.assertRaises(Exception,con.createIndex("medical_info_extraction","discard"))
-        self.assertRaises(Exception,con.createIndex("medical_info_extraction","keep"))
+        con = EsConnection({"host": "localhost", "port": 9200})
+        self.assertRaises(Exception, con.createIndex("medical_info_extraction","discard"))
+        self.assertRaises(Exception, con.createIndex("medical_info_extraction","keep"))
         print "not tested yet"
 
     def test_update(self):
@@ -21,7 +21,7 @@ class TestESconnection(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-#other useful tests:
-#addTypeEqualityFunc(typeobj, function) to test if two objects of same type-class
-#assertListEqual(list1, list2, msg=None)
-#assertTupleEqual(tuple1, tuple2, msg=None)
+# other useful tests:
+# addTypeEqualityFunc(typeobj, function) to test if two objects of same type-class
+# assertListEqual(list1, list2, msg=None)
+# assertTupleEqual(tuple1, tuple2, msg=None)
