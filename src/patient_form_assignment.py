3e90bd3bf3c4ca8efe7fc0864529563c43ef1b8f
christinazavou <czavou01@gmail.com>
2016-12-09 15:26:26 +0100

    accept whole patient

--- src/patient_form_assignment.py
+++ src/patient_form_assignment.py
@@ -5,18 +5,18 @@ import json
 class PatientFormAssignment(object):
 
     def __init__(self, patient, form):
-        self.patient_id = patient.id
+        self.patient = patient
         self.form = form
         self.fields_assignments = []
 
     def get_field_assignment(self, field_name):
         for field_assignment in self.fields_assignments:
-            if field_name in field_assignment.keys():
+            if field_name in field_assignment.to_voc().keys():
                 return field_assignment
         return None
 
     def to_voc(self):
-        voc = {'patient': self.patient_id, 'form': self.form.id, 'assignments': [fa.to_voc() for fa in self.fields_assignments]}
+        voc = {'patient': self.patient.id, 'form': self.form.id, 'assignments': [fa.to_voc() for fa in self.fields_assignments]}
         return voc
 
     def to_json(self):
@@ -29,7 +29,6 @@ class PatientFormAssignment(object):
 class FieldAssignment(object):
 
     def __init__(self, field_name, value, score=None, hit=None, comment=None):
-        # self.__dict__ = {field_name: {'value': value, 'score': score, 'hit': hit, 'comment': comment}}
         self.field_name = field_name
         self.value = value
         self.score = score
